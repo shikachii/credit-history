c7133f9e720faa945136da12b0c74dee22f47f72
shikachii <shikachii095@gmail.com>
2023-06-12 22:47:13 +0900

    :sparkles: AWS Lambdaコード追加

---
+++ lambda/src/go.mod
@@ -0,0 +1,5 @@
+module credit-history
+
+go 1.13
+
+require github.com/aws/aws-lambda-go v1.41.0
---
+++ lambda/src/handler.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func hello() (string, error) {
+	return "Hello lambda", nil
+}
+
+func main() {
+	lambda.Start(hello)
+}
