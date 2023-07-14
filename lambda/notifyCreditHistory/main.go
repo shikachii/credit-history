c2ee5c6cfa7f02f4a953ebc54db597108413395e
shikachii <shikachii095@gmail.com>
2023-07-14 19:48:15 +0900

    :sparkles: slack通知実装

--- lambda/notifyCreditHistory/main.go
+++ lambda/notifyCreditHistory/main.go
@@ -10,14 +10,29 @@ import (
 )
 
 func Handler(ctx context.Context) ([]model.CreditHistory, error) {
-	
+	// 前日と当日の0:00のCreditHistoryを取得する
 	chs, err := lib.ScanBetweenTimestamp("credit-history", "1689174000", "1689260400")
+
+	// 3日前から当日の0:00のCreditHistoryを取得する
+	// chs, err := lib.ScanBetweenTimestamp("credit-history", "1689087600", "1689260400")
+
 	if err != nil {
 		return nil, fmt.Errorf("dynamoDB error: %w", err)
 	}
 
 	for _, ch := range *chs {
 		fmt.Println(ch)
+	}
+
+	// slackに通知する
+	sm := lib.SlackMessage{
+		Channel:   "#credit-history",
+		Username:  "notifyCreditHistory",
+		Text:      "test",
+		IconEmoji: ":ghost:",
+	}
+	if err := lib.SendSlackMessage(sm); err != nil {
+		return nil, fmt.Errorf("slack error: %w", err)
 	}
 
 	return *chs, nil
---
+++ lib/slack.go
@@ -0,0 +1,34 @@
+package lib
+
+import (
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+// SlackMessage is a struct for sending a message to Slack
+type SlackMessage struct {
+	Channel   string
+	Username  string
+	Text      string
+	IconEmoji string
+}
+
+var WEBHOOK_URL = os.Getenv("WEBHOOK_URL")
+
+// SendSlackMessage sends a message to Slack using the Webhook URL
+func SendSlackMessage(sm SlackMessage) error {
+	attachment := slack.Attachment{
+		AuthorName:    "notifyCreditHistory",
+		// AuthorIcon:    "https://avatars2.githubusercontent.com/u/652790",
+		Text:          sm.Text,
+	}
+	msg := slack.WebhookMessage{
+		Attachments: []slack.Attachment{attachment},
+	}
+
+	if err := slack.PostWebhook(WEBHOOK_URL, &msg); err != nil {
+		return err
+	}
+	return nil
+}
