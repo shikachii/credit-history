89ef561b2841c5f2e66e504e458ec8974e86bff7
watanabe-mizuki1 <watanabe-mizuki1@dmm.com>
2024-02-05 00:24:52 +0900

    chore: エラー対応のため@rollup/plugin-commonjsを追加

--- record/rollup.config.mjs
+++ record/rollup.config.mjs
@@ -1,20 +1,21 @@
 import { babel } from "@rollup/plugin-babel";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const extensions = [".ts", ".js"];
 
 const preventTreeShakingPlugin = () => {
-    return {
-      name: 'no-treeshaking',
-      resolveId(id, importer) {
-        if (!importer) {
-            // let's not theeshake entry points, as we're not exporting anything in Apps Script files
-          return {id, moduleSideEffects: "no-treeshake" }
-        }
-        return null;
+  return {
+    name: 'no-treeshaking',
+    resolveId(id, importer) {
+      if (!importer) {
+          // let's not theeshake entry points, as we're not exporting anything in Apps Script files
+        return {id, moduleSideEffects: "no-treeshake" }
       }
+      return null;
     }
   }
+}
 
 export default {
   input: "./src/index.ts",
@@ -23,6 +24,7 @@ export default {
     format: "esm",
   },
   plugins: [
+    commonjs(),
     preventTreeShakingPlugin(),
     nodeResolve({
       extensions,
